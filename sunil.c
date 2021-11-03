a5e1242974c881a79b9f010c6904132b15efa15d
panathalasunilkumar <93139903+panathalasunilkumar@users.noreply.github.com>
2021-11-03 11:33:14 +0530

    Add files via upload

---
+++ sunil.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{  
+    int a,b,c;
+    
+    a=4;
+    b=a*a;
+    c=4*a;
+     
+     printf("area of the square with side %d is %d:\n",a,b);
+     printf("perimeter of the square with side %d is %d:\n",a,c);
+}
